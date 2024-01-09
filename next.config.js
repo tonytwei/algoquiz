e207f19b832a4daf2293726126c689e4ad1596dc
tonytwei <tony.t.wei@gmail.com>
2024-01-10 01:35:23 +1100

    amplify env variables

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ['mongoose'],
-    },
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  env: {
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    DEFAULT_QUESTION_ID: process.env.DEFAULT_QUESTION_ID,
+    MONGO_URI: process.env.MONGO_URI,
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
